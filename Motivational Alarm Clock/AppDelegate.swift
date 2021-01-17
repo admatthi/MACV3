c7eae1270a3a665a84ab886df669224a45316e89
talal ahmad <talalahmad321@gmail.com>
2021-01-17 17:18:29 +0500

    minor fixes

--- Motivational Alarm Clock/AppDelegate.swift
+++ Motivational Alarm Clock/AppDelegate.swift
@@ -305,7 +305,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, AVAudioPlayerDelegate, Al
         do {
             try session.setCategory(
                 .playback,
-                options: .defaultToSpeaker)
+                options: [])
         } catch let setCategoryError {
             // handle error
         }
