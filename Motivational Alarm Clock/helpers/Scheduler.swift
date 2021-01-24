4e0b20511f70e549a33c138085a09a8156a6092e
talal ahmad <talalahmad321@gmail.com>
2021-01-25 01:30:17 +0500

    - Add Smiley Face to Notifications

--- Motivational Alarm Clock/helpers/Scheduler.swift
+++ Motivational Alarm Clock/helpers/Scheduler.swift
@@ -118,7 +118,7 @@ class Scheduler : AlarmSchedulerDelegate
     
     internal func setNotificationWithDate(_ date: Date, onWeekdaysForNotify weekdays:[Int], snoozeEnabled:Bool,  onSnooze: Bool, soundName: String, index: Int) {
         let AlarmNotification: UILocalNotification = UILocalNotification()
-        AlarmNotification.alertBody = "Good Morning"
+        AlarmNotification.alertBody = "Good Morning 😊"
         AlarmNotification.alertAction = "Open App"
         AlarmNotification.category = "myAlarmCategory"
         AlarmNotification.soundName = soundName + ".mp3"
