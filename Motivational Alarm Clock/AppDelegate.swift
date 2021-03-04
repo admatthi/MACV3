1a699dcef1f0221e06d2bd12469298027f33be7a
talal ahmad <talalahmad321@gmail.com>
2021-03-05 01:58:20 +0500

    notification text updated as alarmy

--- Motivational Alarm Clock/AppDelegate.swift
+++ Motivational Alarm Clock/AppDelegate.swift
@@ -615,7 +615,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, AVAudioPlayerDelegate, Al
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         let AlarmNotification: UILocalNotification = UILocalNotification()
-        AlarmNotification.alertBody = "Notification sound will not be work in silent mode as you forcefully killed the app."
+        AlarmNotification.alertTitle = "Opps, you terminated Wake!"
+        AlarmNotification.alertBody = "Your Alarm may not ring, Please open Wake So that your upcomming alarm can ring with sound."
         AlarmNotification.fireDate = Date().addingTimeInterval(1)
         UIApplication.shared.scheduleLocalNotification(AlarmNotification)
     }
